f3b427ca465c5725733fd5e1282132462bf35178
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-01 10:56:35 +0200

    Add files via upload

---
+++ eerste_javascript/script.js
@@ -0,0 +1,32 @@
+console.log("hello Winc Academy")
+let firstName = 'newien';
+//firstName = "newien";
+console.log(firstName)
+// let if;
+//if = "not allowed";
+let sum = 4 + 4;
+console.log(sum)
+
+let sumString = '4' + '4';
+console.log(sumString)
+
+let addition = 1 + 1;
+console.log(addition)
+
+let subtraction = 10 - 5;
+console.log(subtraction)
+
+let multiplication = 2 * 5;
+console.log(multiplication)
+
+let division = 10 / 2;
+console.log(division)
+
+let modulus = 10 % 3;
+console.log(modulus)
+
+let age = 43;
+console.log(age)
+
+let ageWritten = 'forthythree';
+console.log(ageWritten)
